26428a1e774d5cca134ada7586b87d12e4a8308e
Zach Boyd <zachboyd@users.noreply.github.com>
2016-10-21 20:28:21 -0500

    updated to use IPIdeaListActions comp

--- src/aura/IPIdeaListActions/IPIdeaListActionsHelper.js
+++ src/aura/IPIdeaListActions/IPIdeaListActionsHelper.js
@@ -1,6 +1,6 @@
-({
+({    
     getZone : function( component, zoneId, callback ) {
-
+        
         // get apex controller action
         var action = component.get( "c.getZone" );
 
@@ -14,11 +14,8 @@
             var state = response.getState();
             var result = response.getReturnValue();
 
-            if( state === "SUCCESS" ) {
+            if( state == "SUCCESS" ) {
                 callback( result );
-            }
-            else {
-            	console.log( "Error getting zone, zoneId=" + zoneId );
             }
 
         });
@@ -26,19 +23,19 @@
         $A.enqueueAction( action );
 
     },
-
+    
     refreshIdeaList : function( component ) {
-
+                
         var refreshEvent = $A.get( "e.c:IPIdeaListRefreshEvent" );
-
+        
         refreshEvent.setParams({
             sort : component.get("v.sort"),
             filter : component.get("v.filter")
         });
-
+        
         // refresh the idea list
         refreshEvent.fire();
-
+        
     }
-
+    
 })
